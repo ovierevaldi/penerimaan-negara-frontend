5517b1d7c1d80977987cdc5c9271013e94168be3
Ovie Revaldi <ovierevaldi@gmail.com>
2024-08-31 20:08:30 +0700

    Add login call backend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,34 @@
+'use client'
+
 import { redirect } from "next/navigation";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function LoginPage() {
+    // const [posts, setPosts] = useState([]);
+    const requestLogin = async (event) => {
+        event.preventDefault();
+        try{
+            const response = await  
+                axios.post('http://localhost:3000/auth/login',{
+                    username: '11111111',
+                    password: 'abc123'
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+        catch(err){
+
+        }
     // const login = true;
     // if (!login) {
     //     redirect('dashboard')
     //   }
+    }
       
     return(
     <div className="container">
@@ -19,7 +43,7 @@ export default function LoginPage() {
                         <span className="input-group-text" id="input-password">Password</span>
                         <input type="password" className="form-control" />
                     </div>
-                        <button type="submit" className="btn btn-primary w-100">Login</button>
+                        <button type="button" className="btn btn-primary w-100" onClick={requestLogin}>Login</button>
                 </form>
             </div>
     )
